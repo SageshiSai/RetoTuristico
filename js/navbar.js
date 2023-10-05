ca1b73f832469a94b3c01f82a3eded623a9a6388
aitorleon <a_leon@fpzornotza.com>
2023-10-05 09:00:41 +0200

    commit navbar Acabada

--- js/navbar.js
+++ js/navbar.js
@@ -1,23 +1,25 @@
 multilineString = 
-'<div id="navbar" class="sticky-top">'+
-      '<nav class="navbar-container container">'+
-        '<a href="index.html" class="home-link">'+
-          '<div class="navbar-logo">'+
-          '<img class="fotoNavBar " src="img/logo.png" alt="">'+
-          '</div>'+
-          'PIZIA'+
-        '</a>'+
-        '<button type="button" id="navbar-toggle" aria-controls="navbar-menu" aria-label="Toggle menu" aria-expanded="false">'+
-          '<span class="icon-bar"></span>'+
-          '<span class="icon-bar"></span>'+
-          '<span class="icon-bar"></span>'+
-        '</button>'+
-        '<div id="navbar-menu" aria-labelledby="navbar-toggle">'+
-          '<ul class="navbar-links">'+
-            '<li class="navbar-item"><a class="navbar-link" href="/about">Compra</a></li>'+
-            '<li class="navbar-item"><a class="navbar-link" href="/about">Contacto</a></li>'+
-          '</ul>'+
-        '</div>'+
-      '</nav>'+
-    '</div>';
+'<header class="d-flex align-items-center justify-content-evenly sticky-top" id="navbar">' +
+        '<nav class="navbar w-100">' +
+            '<div class="container-fluid">'+
+            '<li style="margin-left: 100px" class="nav-item">'+
+            '<a class="nav-link active" aria-current="page" href="#">Compras</a>'+
+          '</li>'+
+              '<!--<ul class="navbar-nav">'+
+                '<li class="nav-item">'+
+                    '<a class="nav-link active" aria-current="page" href="#">Home</a>'+
+                '</li>'+
+              '</ul>-->'+
+              '<a href="/" class="home-link">'+
+                '<div class="navbar-logo">'+
+                '<img src="img/logo.png">'+
+                '</div>'+
+                'Website Name'+
+              '</a>'+
+              '<li style="margin-right: 100px" class="nav-item">'+
+                '<a class="nav-link active" aria-current="page" href="#">Reclamaciones</a>'+
+              '</li>'+
+            '</div>'+
+          '</nav>'+   
+    '</header>'
 document.write(multilineString);
